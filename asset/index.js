df3868921df2790e3c9cbcfffa7fa3a480e77105
wurui <wurui.wr@gmail.com>
2016-12-28 17:51:40 +0800

    js

--- asset/index.js
+++ asset/index.js
@@ -1,18 +1,17 @@
-define([],function(){
-  return {
-    init:function($mod){
-      $mod.on('focus','.bt-info',function(e){
+define([], function () {
+    return {
+        init: function ($mod) {
 
-      });
-      $mod.on('click','.bt-back',function(e){
-        var link=this.getAttribute('data-link');
-        if(link){
-          location.href=link;
-        }else{
-          history.back()
+            $mod.on('click', '.bt-back', function (e) {
+                var link = this.getAttribute('data-link');
+                if (link) {
+                    location.href = link;
+                } else {
+
+                    history.go(1 - history.length)
+                }
+
+            })
         }
-
-      })
     }
-  }
 })
